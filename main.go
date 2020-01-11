e574dda06af4d9bb99875e15c4150405759edd35
koolalex <koolalex@foxmail.com>
2020-01-12 00:58:50 +0800

    upd ...

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	//load
 	routes.InitRoute()
 
-	fmt.Printf("App Name： v%v \n", config.Cfg.AppName)
+	fmt.Printf("App Name： %v \n", config.Cfg.AppName)
 	fmt.Printf("Markdown Blog Version: v%v \n", config.Cfg.Version)
 	fmt.Printf("Listen On Port: %v \n", config.Cfg.Port)
 	fmt.Printf("Posts Update GitHookUrl: %v   WebHookSecret:%v \n", config.Cfg.GitHookUrl, config.Cfg.WebHookSecret)
